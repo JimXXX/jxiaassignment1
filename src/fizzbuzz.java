53b6bbfc271bffbce4576b03b0213efb09a23904
Jimxia <xiajxsh@gmail.com>
2016-06-23 01:27:31 -0700

    add validating input

--- src/fizzbuzz.java
+++ src/fizzbuzz.java
@@ -8,10 +8,18 @@ public class fizzbuzz{
     public static void main (String[] args){
 
         int max;
-        Scanner m = new Scanner (System.in);
+        Scanner input = new Scanner (System.in);
         System.out.println("Please enter how many numbers you want the system run: ");
-        max = m.nextInt();
 
+        //Validate user's input is an integer
+        while (!input.hasNextInt()){
+            input.next();
+            System.out.println("Please enter an integer: ");
+        }
+
+        max = input.nextInt(); // get the number
+
+        //Output format
         for(int i=1; i <= max; i++){
             if (i % 5 == 0 && i % 7 == 0){
                 System.out.print("FizzBuzz" + " ");
